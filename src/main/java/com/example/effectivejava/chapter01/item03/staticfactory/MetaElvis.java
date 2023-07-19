ae38b497b7d7fc8145a0059a766e82378495bb18
seungwoo-lee <vugil@naver.com>
2023-07-19 23:12:08 +0900

    feat : 제네릭 싱글톤 팩토리 (24쪽)

--- src/main/java/com/example/effectivejava/chapter01/item03/staticfactory/MetaElvis.java
+++ src/main/java/com/example/effectivejava/chapter01/item03/staticfactory/MetaElvis.java
@@ -11,7 +11,7 @@ public class MetaElvis<T> {
     /*
     static 뒤에 제네릭이 또 선언되어 있는 이유
     클래스 레벨의 스코프가 아니다. 메서드 레벨의 스코프이다.
-    메서드 레벨의 제네릭 타입 매개변수는 해당 메서드가 호출될 때 어떤 타입을 사용할지 결정하는 것이기 때문에 호출 시점에 타입이 결정됨.
+    메서드 레벨의 제네릭 타입 매개변수는 해당 메서드가 호출될 때 어떤 타입을 사용할지 결정하는 것이기 때문에 호출 시점에 타입이 결정됨
     */
     public static <T> MetaElvis<T> getInstance() { return (MetaElvis<T>) INSTANCE; }
 
