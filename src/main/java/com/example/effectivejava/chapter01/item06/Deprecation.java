44fa3bbb3f3940e8e5bb81e24ab021abf2d4359b
seungwoo-lee <vugil@naver.com>
2023-07-30 21:32:13 +0900

    feat : item06 (Deprecation / 정규표현, 객체 재활용, 오토박싱 /언박싱) 학습ㅏ

---
+++ src/main/java/com/example/effectivejava/chapter01/item06/Deprecation.java
@@ -0,0 +1,17 @@
+package com.example.effectivejava.chapter01.item06;
+public class Deprecation {
+
+    /**
+     * @deprecated in favor of
+     * {@link #Deprecation(String)}
+     */
+    @Deprecated(forRemoval = true, since = "1.2")
+    public Deprecation() {
+    }
+
+    private String name;
+
+    public Deprecation(String name) {
+        this.name = name;
+    }
+}
